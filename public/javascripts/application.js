1bb2ec7498762e06bc388442212815c5b1a7301d
Phanendar Chitraju <phanendar@gmail.com>
2015-03-06 08:27:25 -0500

    adding changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
-// Javascript Goes Here~
-//
+(document).ready(function(){
+var person = function() {
+
+    var individual = function(type) {
+    return "The Students first name is  " + first;
+  };
+   var color = function(color) {
+    return "this individual has an eye color of " + eyecolor + "eyes";
+  };
+   return {individual: individual, color: color};
+}();
+
+    console.log(person.first);
+    console.log(person.last);
+    console.log(person.individual("kiran"));
+    console.log(person.color("red"));
+});
+
